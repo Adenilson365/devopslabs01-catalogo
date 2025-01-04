40748debd1c6541b8f68bb7ab55829be8abf5881
Adenilson <adekonzelmann@gmail.com>
2025-01-04 09:30:00 -0300

    fix: Altera endpoint para variáveis de ambiente

--- instrumentation.js
+++ instrumentation.js
@@ -14,13 +14,13 @@ const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: new OTLPTraceExporter({
     // optional - default url is http://localhost:4318/v1/traces
-    url: 'http://otel:4318/v1/traces',
+    url: process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT,
     // optional - collection of custom headers to be sent with each request, empty by default
     headers: {},
   }),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter({
-      url: 'http://otel:4318/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+      url: process.env.OTEL_EXPORTER_OTLP_METRICS_ENDPOINT, // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
       headers: {}, // an optional object containing custom headers to be sent with each request
       concurrencyLimit: 1, // an optional limit on pending requests
     }),
